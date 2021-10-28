30eeb80778c98837168ee85c3963f5b48b6abdb0
Ben Hughes <xcyt21@yahoo.com>
2021-10-28 13:06:56 -0600

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Appt',
+        day: 'Today',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'dentist Appt',
+        day: 'Tomorrow',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'dr Appt',
+        day: '3 Weeks',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'dr Appt',
+        day: '3 Weeks',
+        reminder: true,
+    }
+];
